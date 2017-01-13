acaa982751d52b6909f38e406f2c03eae4d17776
Gbekosa <aisosaefemwonkieke@gmail.com>
2017-01-13 09:14:56 -0800

    Updated code

--- SortingAlgorithm/src/Sorting/SortingAlgorithm.java
+++ SortingAlgorithm/src/Sorting/SortingAlgorithm.java
@@ -5,6 +5,8 @@
  */
 package Sorting;
 
+import java.util.Arrays;
+
 /**
  *
  * @author isosceles
@@ -16,10 +18,10 @@ public class SortingAlgorithm {
      */
     public static void main(String[] args) {
         int cSum = 0;
-        int gSum = 0;
+        int aSum = 0;
         int qSum = 0;
         int qHSum = 0;
-        int iter = 20;
+        int iter = 10;
         int arrSize = 10000000;
         for (int i = 0; i < iter; i++) {
             int[] a = new int[arrSize];
@@ -28,16 +30,22 @@ public class SortingAlgorithm {
             int[] d = new int[arrSize];
 
             for (int x = 0; x < arrSize; x++) {
-                a[x] = (int) (Math.random() * arrSize);
+                a[x] = (int) (Math.random() * Integer.MAX_VALUE * 2 
+                        - Integer.MAX_VALUE);
                 b[x] = a[x];
                 c[x] = a[x];
                 d[x] = a[x];
             }
-
-            CombSort cSorter = new CombSort();
             long start = System.currentTimeMillis();
-            cSorter.sort(a);
+            Arrays.sort(d);
             long end = System.currentTimeMillis();
+            System.out.println("Arrays.sort() done in approx. " + ((end - start))
+                    + " milliseconds");
+            aSum += (end - start);
+            CombSort cSorter = new CombSort();
+            start = System.currentTimeMillis();
+            cSorter.sort(a);
+            end = System.currentTimeMillis();
             cSum += (end - start);
             System.out.println("combsort done in approx. " + ((end - start))
                     + " milliseconds");
@@ -49,7 +57,7 @@ public class SortingAlgorithm {
             //int[] tinyTest = { 3 , 4 , 1 , 2 };
             QuickSort qSorter = new QuickSort();
             start = System.currentTimeMillis();
-            qSorter.sort(c);
+            qSorter.sort(b);
             end = System.currentTimeMillis();
             System.out.println("Quicksort(Lomuto Scheme) done in approx. " + ((end - start))
                     + " milliseconds");
@@ -58,7 +66,7 @@ public class SortingAlgorithm {
             //int[] tinyTest = { 3 , 4 , 1 , 2 };
             QuickSortH qHSorter = new QuickSortH();
             start = System.currentTimeMillis();
-            qHSorter.sort(b);
+            qHSorter.sort(c);
             end = System.currentTimeMillis();
             System.out.println("Quicksort(Hoares Scheme) done in approx. " + ((end - start))
                     + " milliseconds");
@@ -79,10 +87,10 @@ public class SortingAlgorithm {
             System.out.println(); */
 
         }
-        System.out.println("Ccombsort avg: " + cSum / iter + " milliseconds");
+        System.out.println("combsort avg: " + cSum / iter + " milliseconds");
         System.out.println("Quicksort(Lomuto Scheme) avg: " + qSum / iter + " milliseconds");
         System.out.println("Quicksort(Hoares Scheme) avg: " + qHSum / iter + " milliseconds");
-       // System.out.println("Gnomesort avg: " + gSum / iter + " milliseconds");
+        System.out.println("Array.sort() avg: " + aSum / iter + " milliseconds");
 
     }
 }
